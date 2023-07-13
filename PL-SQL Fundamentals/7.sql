0fba7e39fa34c4077bdca671305ddcd00fd494f7
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2023-07-13 12:11:38 +0300

    Create 7.sql

---
+++ PL-SQL Fundamentals/7.sql
@@ -0,0 +1,8 @@
+DECLARE
+  maximum_deposit     CONSTANT REAL    := 25000.00;  -- SQL data type
+  min_no_of_days CONSTANT INTEGER := 75;      -- SQL data type
+  nominee_yn     CONSTANT BOOLEAN := FALSE;    -- PL/SQL-only data type
+BEGIN
+  NULL;
+END;
+/
